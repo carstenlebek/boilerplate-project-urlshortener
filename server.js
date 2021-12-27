fce9aef2daee54b96910a997bd19793613731151
konzeptfabrik <carsten.lebek@googlemail.com>
2021-12-27 13:23:50 +0100

    done

--- server.js
+++ server.js
@@ -1,24 +1,27 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
 const app = express();
+var bodyParser = require("body-parser");
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
+
 app.use(cors());
 
-app.use('/public', express.static(`${process.cwd()}/public`));
+app.use("/public", express.static(`${process.cwd()}/public`));
 
-app.get('/', function(req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+app.get("/", function (req, res) {
+	res.sendFile(process.cwd() + "/views/index.html");
 });
 
-// Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
+app.post("/api/shorturl", urlencodedParser, function (req, res) {
+	console.log(req.body);
+	const originalUrl = req.body.url;
 });
 
-app.listen(port, function() {
-  console.log(`Listening on port ${port}`);
+app.listen(port, function () {
+	console.log(`Listening on port ${port}`);
 });
